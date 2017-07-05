0ec063e43776169e4762730b498447dd78c7c38f
David Sawatske <sawatskeda10@gmail.com>
2017-07-05 11:27:10 -0700

    Created book list container with react-redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books.js'
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  books: BooksReducer;
+  books: BooksReducer
 });
 
 export default rootReducer;
-3
